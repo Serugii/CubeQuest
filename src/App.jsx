7c51cb9af07cdef3dd4f2cd47f96b0a0a00a9d74
Sluhanskyi <luhanskyi.serhii@student.uzhnu.edu.ua>
2024-10-22 02:09:19 +0300

    Created folder structure, completed Buchuk's code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
 
-// import resizeHook from '@hooks/resize';
-// import styles from './App.css';
-// import * as styles from './App.css';
-import Header from './components/Header/Header';
-
 const App = () => {
     return (
-        <div>
-            <Header />
-            <div id="tabs">
-                <menu>
-                    <button id="btn-why-react" className="active">
-                        Чому React?
-                    </button>
-                    <button id="btn-core-features">Основні переваги (Features)</button>
-                    <button id="btn-resources">Корисні посилання</button>
-                </menu>
-                <div id="tab-content"></div>
-            </div>
+        <div data-testid="app">
+            <h1>CubeQuest</h1>
         </div>
     );
 };
 
-// export { App };
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,7 @@ import styles from './Header.css';
 // function Header() {};
 const Header = () => (
     <header className={styles.container}>
-        <div>
-            <h1 className="header">Vanilla JavaScript</h1>
-            <p>тобто, JavaScript без будь яких (UI) фреймворків або бібліотек</p>
-            <p>чистий JavaScript</p>
-        </div>
+        <div></div>
     </header>
 );
 
